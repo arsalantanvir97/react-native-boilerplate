710341c5cab0d5400177e23897d11a032f357776
Arsalan <arsalantanvir3@gmail.com>
2021-06-17 17:43:56 +0500

    ui completed

--- android/app/src/main/java/com/frontoffice/MainActivity.java
+++ android/app/src/main/java/com/frontoffice/MainActivity.java
@@ -5,10 +5,18 @@ import com.facebook.react.ReactActivity;
 
 // react-native-splash-screen >= 0.3.1 
 import org.devio.rn.splashscreen.SplashScreen; // here 
+import android.content.Intent;
+import android.content.res.Configuration;
  
 
 public class MainActivity extends ReactActivity {
-
+   @Override
+   public void onConfigurationChanged(Configuration newConfig) {
+       super.onConfigurationChanged(newConfig);
+       Intent intent = new Intent("onConfigurationChanged");
+       intent.putExtra("newConfig", newConfig);
+       this.sendBroadcast(intent);
+   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
